f09b0c057c90f1115d203215b32ed2db72fcca85
rj-programing <rajdeep.programing@gmail.com>
2024-02-02 17:45:13 +0530

    Added container

--- instagram_clone/lib/main.dart
+++ instagram_clone/lib/main.dart
@@ -11,23 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
 
-  }
-}
-
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
+        home: Scaffold(
+          body: Container(),
+        )
 
     );
-
+  }
 }
